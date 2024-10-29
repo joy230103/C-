de1690e846c4a68d21d32dfbd40cf6d45173dd7a
Nana <nguyenphuonganh231437@gmail.com>
2024-10-29 12:04:07 +0700

    sửa lần 1

--- if-else.cpp
+++ if-else.cpp
@@ -13,43 +13,87 @@ int main()
   // cout << "Gia tri y can tim la: " << (tong - h) / 2 << endl;
 
   // 2. Tinh BMI
-  float cao, nang, BMI;
-  cout << "Nhap chieu cao: ";
-  cin >> cao;
-  cout << "Nhap can nang: ";
-  cin >> nang;
-  BMI = nang / (cao * 2);
-  cout << "BMI của bạn = " << BMI << endl;
-  if (BMI < 15)
-  {
-    cout << "Than hinh qua gay";
-  }
-  else if (BMI >= 15 && BMI < 16)
-  {
-    cout << "Than hinh gay";
-  }
-  else if (BMI >= 15 && BMI < 16)
-  {
-    cout << "Than hinh gay";
-  }
-  else if (BMI >= 16 && BMI < 18.5)
-  {
-    cout << "Than hinh hoi gay";
-  }
-  else if (BMI >= 18.5 && BMI < 25)
-  {
-    cout << "Than hinh binh thuong";
-  }
-  else if (BMI >= 25 && BMI < 30)
-  {
-    cout << "Than hinh hoi beo";
-  }
-  else if (BMI >= 30 && BMI < 35)
-  {
-    cout << "Than hinh beo";
-  }
-  else
-  {
-    cout << "Than hinh qua beo";
-  }
+  // float cao, nang, BMI;
+  // cout << "Nhap chieu cao: ";
+  // cin >> cao;
+  // cout << "Nhap can nang: ";
+  // cin >> nang;
+  // BMI = nang / (cao * 2);
+  // cout << "BMI của bạn = " << BMI << endl;
+  // if (BMI < 15)
+  // {
+  //   cout << "Than hinh qua gay";
+  // }
+  // else if (BMI >= 15 && BMI < 16)
+  // {
+  //   cout << "Than hinh gay";
+  // }
+  // else if (BMI >= 15 && BMI < 16)
+  // {
+  //   cout << "Than hinh gay";
+  // }
+  // else if (BMI >= 16 && BMI < 18.5)
+  // {
+  //   cout << "Than hinh hoi gay";
+  // }
+  // else if (BMI >= 18.5 && BMI < 25)
+  // {
+  //   cout << "Than hinh binh thuong";
+  // }
+  // else if (BMI >= 25 && BMI < 30)
+  // {
+  //   cout << "Than hinh hoi beo";
+  // }
+  // else if (BMI >= 30 && BMI < 35)
+  // {
+  //   cout << "Than hinh beo";
+  // }
+  // else
+  // {
+  //   cout << "Than hinh qua beo";
+  // }
+
+  // 3.Kiem tra nam nhuan
+  // int nam;
+  // cout << "Nhap vao nam ban muon kiem tra: ";
+  // cin >> nam;
+  // if ((nam % 4 == 0) && (nam % 100 != 0) || (nam % 400 == 0))
+  // {
+  //   cout << "Nam " << nam << " la nam nhuan" << endl;
+  //   ;
+  // }
+  // else
+  // {
+  //   cout << "Nam " << nam << " khong la nam nhuan" << endl;
+  // }
+
+  // 4. Nhap thang de biet co bao nhieu ngay
+  // int thang, ngay, nam;
+  // cout << "Nhap vao thang ban muon biet: ";
+  // cin >> thang;
+  // if ((thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12))
+  // {
+  //   cout << "Thang có 31 ngay" << endl;
+  // }
+
+  // else if (thang == 2)
+  // {
+  //   cout << "Nhap them nam: ";
+  //   cin >> nam;
+  //   if ((nam % 4 == 0) && (nam % 100 != 0) || (nam % 400 == 0))
+  //   {
+  //     cout << "Thang co 29 ngay";
+  //   }
+  //   else
+  //   {
+  //     cout << "Thang co 28 ngay";
+  //   }
+  // }
+
+  // else
+  // {
+  //   cout << "Thang co 30 ngay";
+  // }
+
+  // 5. Giai phuong trinh
 }
